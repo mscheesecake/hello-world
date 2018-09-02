90dd865497c4e109e05d0e77703b0b4bbb0ceb97
Katarzyna Kutrzuba <katarzyna.kutrzuba@gmail.com>
2018-09-02 15:52:46 +0200

    halfway there, chapter2

--- 02/chapter2.js
+++ 02/chapter2.js
@@ -2,3 +2,27 @@
 
 let x = 30;
 console.log("the value of x is ",x);
+
+let y = 5;
+console.log("If you want to get 100, you need to add " + (100-y) + " " + "to " + y );
+
+let z = 45;
+if (!Number.isNaN(z)){
+	console.log("Your number is " + z);
+}
+
+let a = 3
+if (!Number.isNaN(a)){
+	console.log(a + " is a number.")
+} else {
+	console.log(a + " is not a number");
+}
+
+let c = 98;
+if (c < 10) {
+	console.log("Small");
+} else if (c <100) {
+	console.log("Medium");
+} else {
+	console.log("Large");
+}
