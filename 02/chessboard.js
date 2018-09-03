47534ee70673b740deadd5caf4233fd9838eab9e
Katarzyna Kutrzuba <katarzyna.kutrzuba@gmail.com>
2018-09-03 20:26:27 +0200

    chessboard ultimately approvedby s21n

--- 02/chessboard.js
+++ 02/chessboard.js
@@ -1,11 +1,11 @@
 const boardSize = 8;
-let chessboard = "";
+let chessboard = '';
 
 for (let lineNumber = 1; lineNumber <= boardSize; lineNumber++) {
 	for (let i = 1; i <= boardSize; i++) {
 		chessboard += (lineNumber % 2) 
-			? (i % 2) ? " " : "#"	
-			: (i % 2) ? "#" : " ";
+			? (i % 2) ? ' ' : '#'	
+			: (i % 2) ? '#' : ' ';
 		}
 				
 	chessboard += '\n'; 
