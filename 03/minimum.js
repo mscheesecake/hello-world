983b7a2fe44402cfcbae289aeb238bae3ad1a7dd
Katarzyna Kutrzuba <katarzyna.kutrzuba@gmail.com>
2018-09-08 16:18:47 +0200

    minimum finction

---
+++ 03/minimum.js
@@ -0,0 +1,11 @@
+/* The previous chapter introduced the standard function 
+Math.min that returns its smallest argument. We can build something 
+like that now. 
+Write a function min that takes two arguments and returns their minimum. */
+
+function min(a,b) {
+	return a < b ? a : b
+}
+
+console.log(min(4,5))
+console.log(min(-1,8))
